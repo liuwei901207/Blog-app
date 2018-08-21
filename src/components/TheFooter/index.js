1b294c0794c91152924c1c03d620580488eb63ac
刘伟 <116402157@qq.com>
2018-08-21 09:04:09 +0800

    fet(site-footer): 添加全站底部页脚

--- src/components/TheFooter/index.js
+++ src/components/TheFooter/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
-const Index = () => {
+import styles from './index.css'
+
+const TheFooterCps = () => {
   return (
-    <div className="site-footer">TheFooter</div>
+    <div className={styles['site-footer']}>
+      <div className={styles['site-footer__item']}>
+        <span>本站由 @liuwei 创建 - 主题参考了 <a href="https://hexo.io/" target="_blank" rel="noopener noreferrer">Hexo</a></span>
+        <span id="theme-info"> 的主题 <a href="https://github.com/fi3ework/hexo-theme-archer" target="_blank" rel="noopener noreferrer">Archer</a></span>
+      </div>
+      <div className={styles['site-footer__item']}>
+        <span> © 2018. 粤ICP备17139665号-1 </span>
+      </div>
+      <div className={`${styles['site-footer__item']} ${styles['site-footer__item--pv']}`}>
+        <span> PV: 50000 </span>
+      </div>
+    </div>
   )
 }
 
-export default Index
+export default TheFooterCps
